616e3ca0c2f721267c82af8781aac7ea8835edd4
Boye Oomens <github@boye.io>
2026-08-24 16:15:28 +0200

    chore: release version 1.1.0 with updated server and package metadata

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,7 @@ const client = new EboekhoudenClient(defaultAdministration, credentialsMap);
 
 const server = new McpServer({
   name: 'e-boekhouden-mcp',
-  version: '1.0.0',
+  version: '1.1.0',
 });
 
 registerAuthTools(server, client);
